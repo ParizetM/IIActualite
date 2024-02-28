6aeff3e93a2148b38beea324151fd28b361ed9c2
Parizetm <parizetm@free.fr>
2024-02-28 15:04:57 +0100

    Page principal fini fini

---
+++ includes/index-footer.php
@@ -0,0 +1,9 @@
+<div class="footer_map_container">
+    <iframe width="80%" height="260" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?width=50%25&amp;height=200&amp;hl=en&amp;q=1%20Boulevard%20de%20l'universit%C3%A9,%20Saint%20Nazaire,%20France+(My%20Business%20Name)&amp;t=&amp;z=12&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"><a href="https://www.gps.ie/">gps devices</a></iframe>
+</div>
+<div class="footer_link_container">
+    <a href="pages/contact.php">Nous contacter</a>
+    <a href="pages/mentions-legales.php">mentions légales</a>
+    <a href="pages/CGU.php">CGU</a>
+</div>
+<p>&copy; 2024 IIA Saint-Nazaire. Tous droits réservés.</p>
--- index.php
+++ index.php
@@ -26,21 +26,21 @@ include "includes/debugger.php";
         <h1> Bienvenue sur les actualité de L'IIA</h1>  
         <div class="actu_container">
             <?php 
-                $sql= "SELECT titre,texte,lien_image,date,auteurs.nom,auteurs.prenom FROM actualites,auteurs WHERE id_auteur = auteurs.id ORDER BY date DESC LIMIT 5";
+                $sql= "SELECT titre,texte,lien_image,date,auteurs.nom,auteurs.prenom,actualites.id FROM actualites,auteurs WHERE id_auteur = auteurs.id ORDER BY date DESC LIMIT 5";
                 $resultat = $pdo->query($sql);
                 while ($donnees = $resultat->fetch(PDO::FETCH_ASSOC)) {
-                    echo '<div class="actualite">';
+                    echo '<div class="actualite"><a href="pages/article.php?'.$donnees['id'].'">';
                     echo '<img src="uploads/'.$donnees['lien_image'].'" alt="image article" title="image article">';
                     echo '<div class="actualite_preview_body">';
                     echo '<h2>'.$donnees['titre'].'</h2>';
                     echo '<p>'.substr($donnees['texte'],0,100).'...</p>';
                     echo '<small>'.$donnees['nom'].' '.$donnees['prenom'].' '.$donnees['date'].'</small>';
                     echo '</div>';
-                    echo '</div>';
+                    echo '</div></a>';
                 }
 
              ?> 
-
+                
          </div>
     </main>
     <footer>
